6084e1d629a2c5b1874546062b2875f454462486
Pete Birley <pete@port.direct>
2018-06-11 21:10:15 -0500

    Hack-update

--- quota.sh
+++ quota.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 OS_PROJECT=admin
 openstack quota set ${OS_PROJECT} --cores "$(($(grep -c ^processor /proc/cpuinfo) * 2))"
-openstack quota set ${OS_PROJECT} --ram "$(($(($(awk '/^MemTotal/ { print $(NF-1) }' /proc/meminfo) / 1024)) - 32768 ))"
+openstack quota set ${OS_PROJECT} --ram "$(($(($(awk '/^MemTotal/ { print $(NF-1) }' /proc/meminfo) / 1024)) - 8192 ))"
 openstack quota set ${OS_PROJECT} --instances 64
 openstack quota set ${OS_PROJECT} --secgroups 128
 openstack quota set ${OS_PROJECT} --floating-ips 256
